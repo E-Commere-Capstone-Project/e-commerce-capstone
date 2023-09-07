142b5f2ab29e7709ddb4bfac88477db97aa3f295
fmagmych <mychaelmagnuson@gmail.com>
2023-09-07 13:12:02 -0500

    Adding the baseline components for a navigation, a home page, and a footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import Footer from "./components/Footer.jsx";
+
 import "./App.css";
 
+import { Routes, Route } from "react-router-dom";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
 }
