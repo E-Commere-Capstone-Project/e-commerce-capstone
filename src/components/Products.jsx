adff7de9c93ee03c75a2c2b6e7aacded358fcf23
fmagmych <mychaelmagnuson@gmail.com>
2023-09-07 13:44:21 -0500

    BMaking the products component functional to display a map of all the products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../API/index.js";
+
 export default function Products() {
+  const [products, setProducts] = useState([]);
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    async function ProductsFetch() {
+      try {
+        const data = await fetchProducts();
+        console.log(data);
+        return setProducts(data);
+      } catch (error) {
+        setErr(error);
+        console.log(error);
+      }
+    }
+    ProductsFetch();
+  }, []);
+
+  function handleMapping(data) {
+    return data.map((product) => (
+      <div key={product.id}>
+        <h3>{product.title}</h3>
+        <p>{product.price}</p>
+        <p>{product.description}</p>
+        <img src={product.image} alt={product.title} />
+      </div>
+    ));
+  }
   return (
     <>
       <p>Products</p>
+      <div>{products && handleMapping(products)}</div>
+      {err && <h3>{err}</h3>}
     </>
   );
 }
