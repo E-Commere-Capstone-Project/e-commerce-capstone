da3ec12bbe9caf1935d825659f45bb7627f7d8ac
fmagmych <mychaelmagnuson@gmail.com>
2023-09-07 13:43:21 -0500

    Making a functioning GET request for all products

--- src/API/index.js
+++ src/API/index.js
@@ -1,1 +1,20 @@
 // USE for writing all the api calls
+const API_URL = "https://fakestoreapi.com";
+const PRODUCTS_API_URL = `${API_URL}/products`;
+
+// GET all products
+export async function fetchProducts() {
+  try {
+    const response = await fetch(PRODUCTS_API_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+fetchProducts();
