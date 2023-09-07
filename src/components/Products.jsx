b89eae6e9cb4d1f01eb76bc878bdd272871a0609
fmagmych <mychaelmagnuson@gmail.com>
2023-09-07 16:15:10 -0500

    Building a working sorter to be able to sort through the array of products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { fetchProducts } from "../API/index.js";
 import { Link, useNavigate } from "react-router-dom";
+import { orderBy } from "lodash";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [err, setErr] = useState(null);
+  const [sorter, setSorter] = useState("id");
+  const [order, setOrder] = useState("asc");
 
   const navigate = useNavigate();
 
@@ -24,21 +27,42 @@ export default function Products() {
 
   function handleMapping(data) {
     return data.map((product) => (
-      <div key={product.id}>
+      <div key={product.id} className="product">
+        <Link to={`/products/${product.id}`}>View Product</Link>
         <h3 onClick={() => navigate(`/products/${product.id}`)}>
           {product.title}
         </h3>
-        <Link to={`/products/${product.id}`}>View Product</Link>
-        <p>{product.price}</p>
-        <p>{product.description}</p>
-        <img src={product.image} alt={product.title} />
+        <p>${product.price}</p>
+        {/* <p>{product.description}</p> */}
+        {/* <img src={product.image} alt={product.title} /> */}
       </div>
     ));
   }
+
+  const sortedProducts = orderBy(products, [sorter], [order]);
+
+  console.log(sortedProducts);
   return (
     <>
       <p>Products</p>
-      <div>{products && handleMapping(products)}</div>
+      <div>
+        <h3>Sort by: </h3>
+        <button onClick={() => setOrder(order === "asc" ? "desc" : "asc")}>
+          {order.charAt(0).toUpperCase() + order.slice(1)}
+        </button>
+        <button onClick={() => setSorter("title")}>Name</button>
+        <button onClick={() => setSorter("price")}>Price</button>
+        <button
+          onClick={() => {
+            setSorter("id");
+            setOrder("asc");
+          }}
+        >
+          Clear sort
+        </button>
+      </div>
+      <div>{products && handleMapping(sortedProducts)}</div>
+
       {err && <h3>{err}</h3>}
     </>
   );
