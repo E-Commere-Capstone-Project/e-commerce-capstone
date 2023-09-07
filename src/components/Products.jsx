7b79d5eb68e0aab7259784f81a628e1812ae6935
fmagmych <mychaelmagnuson@gmail.com>
2023-09-07 14:21:16 -0500

    Making the product.title have an onclick event to navigate to view the single product

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { fetchProducts } from "../API/index.js";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [err, setErr] = useState(null);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function ProductsFetch() {
@@ -23,7 +25,9 @@ export default function Products() {
   function handleMapping(data) {
     return data.map((product) => (
       <div key={product.id}>
-        <h3>{product.title}</h3>
+        <h3 onClick={() => navigate(`/products/${product.id}`)}>
+          {product.title}
+        </h3>
         <Link to={`/products/${product.id}`}>View Product</Link>
         <p>{product.price}</p>
         <p>{product.description}</p>
