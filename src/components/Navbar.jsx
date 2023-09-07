de145ff3978f99131b453f3479ae886fdbdce150
fmagmych <mychaelmagnuson@gmail.com>
2023-09-07 13:21:25 -0500

    Adding a basic navigation to be able to move between available pages using the UI during the development process

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
-    <>
-      <p>Navigation</p>
-    </>
+    <header>
+      <h1>Website Name Here</h1>
+      <div>
+        <nav>
+          <Link to="/">
+            <h3>Home</h3>
+          </Link>
+          <Link to="/products">
+            <h3>Products</h3>
+          </Link>
+        </nav>
+      </div>
+    </header>
   );
 }
