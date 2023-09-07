8a6ad669e62ef191f6bc2ed2af65dfb77e97b11b
fmagmych <mychaelmagnuson@gmail.com>
2023-09-07 14:16:59 -0500

    Building a functional component for viewing a single product

--- src/components/ViewProduct.jsx
+++ src/components/ViewProduct.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { fetchOneProduct } from "../API/index.js";
+
 export default function ViewProduct() {
+  const [product, setProduct] = useState([]);
+  const [error, setError] = useState(null);
+
+  let { productId } = useParams();
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    async function ProductFetch() {
+      try {
+        const data = await fetchOneProduct(productId);
+        console.log(data);
+        return setProduct(data);
+      } catch (error) {
+        setError(error);
+        console.error(error);
+      }
+    }
+    ProductFetch();
+  }, [productId]);
   return (
     <>
       <p>View Product</p>
+      <Link to="/products">
+        <p>Back</p>
+      </Link>
+      {product && <p>{product.title}</p>}
+      {error && <p>{error}</p>}
     </>
   );
 }
