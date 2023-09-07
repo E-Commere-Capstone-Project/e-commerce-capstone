adaec13e35c83cc40e4bb5b9b430d87d82e15e0a
fmagmych <mychaelmagnuson@gmail.com>
2023-09-07 18:47:31 -0500

    Getting a sort and filter function working

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import { fetchProducts } from "../API/index.js";
 import { Link, useNavigate } from "react-router-dom";
-import { orderBy } from "lodash";
+import { orderBy, filter } from "lodash";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [err, setErr] = useState(null);
   const [sorter, setSorter] = useState("id");
   const [order, setOrder] = useState("asc");
+  const [category, setCategory] = useState(null);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [priceMin, setPriceMin] = useState(0);
+  const [priceMax, setPriceMax] = useState(10000);
 
   const navigate = useNavigate();
 
@@ -16,7 +20,8 @@ export default function Products() {
       try {
         const data = await fetchProducts();
         console.log(data);
-        return setProducts(data);
+
+        return setProducts(data), setFilteredProducts(data);
       } catch (error) {
         setErr(error);
         console.log(error);
@@ -39,29 +44,131 @@ export default function Products() {
     ));
   }
 
-  const sortedProducts = orderBy(products, [sorter], [order]);
+  function handleSortProducts(sorter, order) {
+    setSorter(sorter);
+    setOrder(order);
+    const sortedProducts = orderBy(
+      filteredProducts === products ? products : filteredProducts,
+      [sorter],
+      [order]
+    );
+    return setFilteredProducts(sortedProducts);
+  }
 
-  console.log(sortedProducts);
+  // const orderButton = order.charAt(0).toUpperCase() + order.slice(1);
+
+  function handleFilterCategory(category) {
+    setFilteredProducts(products);
+    setCategory(category);
+    const filteredCategory = filter(
+      products,
+      (product) => product.category === category
+    );
+    return setFilteredProducts(filteredCategory);
+  }
+
+  // function handleFilterPrice(min, max) {
+  //   setFilteredProducts(products);
+  //   const filteredPrice = filter(
+  //     products,
+  //     (product) => product.price >= min && product.price <= max
+  //   );
+  //   return setFilteredProducts(filteredPrice);
+  // }
+
+  console.log(filteredProducts);
   return (
     <>
       <p>Products</p>
       <div>
         <h3>Sort by: </h3>
-        <button onClick={() => setOrder(order === "asc" ? "desc" : "asc")}>
-          {order.charAt(0).toUpperCase() + order.slice(1)}
-        </button>
-        <button onClick={() => setSorter("title")}>Name</button>
-        <button onClick={() => setSorter("price")}>Price</button>
         <button
           onClick={() => {
-            setSorter("id");
-            setOrder("asc");
+            handleSortProducts(sorter, order === "asc" ? "desc" : "asc");
+          }}
+        >
+          {order === "asc" ? "Desc" : "Asc"}
+        </button>
+        <button
+          onClick={() => {
+            handleSortProducts("title");
+          }}
+        >
+          Name
+        </button>
+        <button
+          onClick={() => {
+            handleSortProducts("price");
+          }}
+        >
+          Price
+        </button>
+        <button
+          onClick={() => {
+            handleSortProducts("id", "asc");
           }}
         >
           Clear sort
         </button>
       </div>
-      <div>{products && handleMapping(sortedProducts)}</div>
+
+      <div>
+        <h3>Filter by: </h3>
+        <p>Category: </p>
+        <button
+          onClick={() => {
+            handleFilterCategory("electronics");
+          }}
+        >
+          Electronics
+        </button>
+        <button
+          onClick={() => {
+            handleFilterCategory("jewelery");
+          }}
+        >
+          Jewelry
+        </button>
+        <button
+          onClick={() => {
+            handleFilterCategory("men's clothing");
+          }}
+        >
+          Men&apos;s Clothing
+        </button>
+        <button
+          onClick={() => {
+            handleFilterCategory("women's clothing");
+          }}
+        >
+          Women&apos;s Clothing
+        </button>
+        <button
+          onClick={() => {
+            setFilteredProducts(products);
+          }}
+        >
+          Clear Category
+        </button>
+        {/* <p>Price: </p>
+        <label>
+          Min:
+          <input
+            type="number"
+            value={priceMin}
+            onChange={(e) => setPriceMin(e.target.value)}
+          />
+        </label>
+        <label>
+          Max:
+          <input
+            type="number"
+            value={priceMax}
+            onChange={(e) => setPriceMax(e.target.value)}
+          />
+        </label> */}
+      </div>
+      <div>{products && handleMapping(filteredProducts)}</div>
 
       {err && <h3>{err}</h3>}
     </>
