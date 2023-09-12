8e485636cca514e6aa6585cef37cfe5670bbf341
fmagmych <mychaelmagnuson@gmail.com>
2023-09-11 21:19:55 -0500

    Adding an id to the parent container for styling; usingg a cbutton parent container for grouping buttons to help with styling; changing the buttons to use a button component from chakra-ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 export default function Navbar() {
   return (
-    <header>
-      <h1>Website Name Here</h1>
+    <header id="navbar">
+      <h1>Status Quo</h1>
       <div>
         <nav>
-          <Link to="/">
-            <h3>Home</h3>
-          </Link>
-          <Link to="/products">
-            <h3>Products</h3>
-          </Link>
+          <ButtonGroup>
+            <Link to="/">
+              <Button variant="solid" colorScheme="blue">
+                Home
+              </Button>
+            </Link>
+            <Link to="/products">
+              <Button variant="solid" colorScheme="blue">
+                Products
+              </Button>
+            </Link>
+          </ButtonGroup>
         </nav>
       </div>
     </header>
