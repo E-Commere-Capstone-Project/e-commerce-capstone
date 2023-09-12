137e654a01385531152500ce58c154c6b247ac84
fmagmych <mychaelmagnuson@gmail.com>
2023-09-11 21:22:10 -0500

    Wrapping the HashRouter component and the App component in the ChakraProvider component to help with the incorporation of chakra components

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <ChakraProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
