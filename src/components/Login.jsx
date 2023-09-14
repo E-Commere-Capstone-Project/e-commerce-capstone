4bfb98a7b73974fe02374aeab73baf125b54ea50
fmagmych <mychaelmagnuson@gmail.com>
2023-09-13 21:35:40 -0500

    Setting the default export and adding sudocode on how to build out the form for logging in

---
+++ src/components/Login.jsx
@@ -0,0 +1,22 @@
+// import useState from react;
+export default function Login() {
+  /*
+  const usernameState;
+  const passwordState;
+
+  return (
+    form to enter username and password in order to log in 
+    <form>
+    <label>
+    Username: 
+     <input value={usernameState} onChange={(e)=>setUsernameState(e.target.value)}/>
+    </label>
+    <label>
+    Password:
+    <input value={passwordState} onChange={e => setPasswordState(e.target.value)}/>
+    </label>
+    <button>Login</button>
+    </form>
+  )
+  */
+}
