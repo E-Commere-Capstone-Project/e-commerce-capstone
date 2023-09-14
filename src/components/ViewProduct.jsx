62c114e81a76ad81c47780938b01550dc5d223ee
fmagmych <mychaelmagnuson@gmail.com>
2023-09-14 09:37:30 -0500

    Adjusting component layout and having the component display more data about the product to help with UI/UX; adding the add to cart button so it is accessible at the product level

--- src/components/ViewProduct.jsx
+++ src/components/ViewProduct.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { fetchOneProduct } from "../API/index.js";
+import { Button, Image } from "@chakra-ui/react";
+import useShopUser from "./context/UserContext.jsx";
 
 export default function ViewProduct() {
   const [product, setProduct] = useState([]);
   const [error, setError] = useState(null);
+  const { addToCart, cart } = useShopUser();
 
   let { productId } = useParams();
   // const navigate = useNavigate();
@@ -22,13 +25,45 @@ export default function ViewProduct() {
     }
     ProductFetch();
   }, [productId]);
+
+  useEffect(() => {
+    console.log(`useEffect`, cart);
+    localStorage.setItem("localCart", JSON.stringify(cart));
+  }, [cart]);
+
   return (
     <>
       <p>View Product</p>
-      <Link to="/products">
-        <p>Back</p>
-      </Link>
-      {product && <p>{product.title}</p>}
+      <Button variant="solid" colorScheme="blue">
+        <Link to="/products">
+          <p>Back</p>
+        </Link>
+      </Button>
+
+      {product && (
+        <div id="product-main-div">
+          <div id="product-image-div">
+            <Image
+              src={product.image}
+              alt={product.title}
+              maxW={{ base: "100%", sm: "450px" }}
+            />
+          </div>
+          <div id="product-content-div">
+            <h2>{product.title}</h2>
+            <p>{product.category}</p>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <Button
+              variant="solid"
+              colorScheme="blue"
+              onClick={() => addToCart(product)}
+            >
+              Add to cart
+            </Button>
+          </div>
+        </div>
+      )}
       {error && <p>{error}</p>}
     </>
   );
