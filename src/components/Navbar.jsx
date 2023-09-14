ede0490a1cc8d62b83d2b0e51c7b04ee44d077ac
fmagmych <mychaelmagnuson@gmail.com>
2023-09-14 11:01:39 -0500

    Making the navbar dynamic to work with the log in state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
 import { Link } from "react-router-dom";
 import { Button, ButtonGroup } from "@chakra-ui/react";
+import { useEffect } from "react";
+import useShopUser from "./context/UserContext.jsx";
 
 export default function Navbar() {
+  const localLoggedIn = localStorage.getItem("isLoggedIn");
+  const localIsLoggedIn = JSON.parse(localLoggedIn);
+
+  const { isLoggedIn, changeIsLoggedIn } = useShopUser();
+
+  // console.log("navbar", isLoggedIn);
+
+  useEffect(() => {
+    // if (isLoggedIn === true) return;
+    console.log(`login state`, isLoggedIn);
+  }, [isLoggedIn]);
+
+  function handleLogOut() {
+    localStorage.setItem("isLoggedIn", JSON.stringify(false));
+    changeIsLoggedIn(false);
+  }
+
   return (
     <header id="navbar">
       <h1>Status Quo</h1>
@@ -18,6 +37,16 @@ export default function Navbar() {
                 Products
               </Button>
             </Link>
+            <Link to={`/users/${isLoggedIn === true ? "Profile" : "login"}`}>
+              <Button variant="solid" colorScheme="blue">
+                {isLoggedIn === true ? "Profile" : "Sign in"}
+              </Button>
+            </Link>
+            {isLoggedIn === true && (
+              <Button variant="solid" colorScheme="blue" onClick={handleLogOut}>
+                Log out
+              </Button>
+            )}
           </ButtonGroup>
         </nav>
       </div>
