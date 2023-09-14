596db73336ebca535ba6865cc4ed2a8c69dfafe8
fmagmych <mychaelmagnuson@gmail.com>
2023-09-13 21:35:08 -0500

    Adding the function default export and all the sudocode to be able to have a path when building the component and different working aspects of the cart page

---
+++ src/components/Cart.jsx
@@ -0,0 +1,27 @@
+// Page should have a mapping of all the cart data and also display quantities with a way to edit the quantity
+// You can remove from cart on this page as well
+// Access to check out from here as well
+
+/*
+Needs to import or receive data in relation to the cart itself and other data relevant
+to the user; cart should have two functionalities, one as a guest checkout and one as a user checkout 
+user should be authenticated for checkout process
+*/
+export default function Cart() {
+  /*
+  
+ cart.map(item => {
+  <>
+  image
+  title
+  way to change the quantity
+  a trash icon to be able to remove from cart
+  </>
+ }) 
+  
+ Display the total and should update in real time as items are added or removed
+
+ button to direct to check out 
+
+  */
+}
