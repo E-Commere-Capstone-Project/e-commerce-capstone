fb4bb04577dd2199a001c6c8ea46f1e1731cbba6
fmagmych <mychaelmagnuson@gmail.com>
2023-09-14 12:58:40 -0500

    Adding most of the working functionality, you can view all the products in the cart, edit the quantity, remove the  product from the cart, view a total for each product and for the entire cart and next will be for checking out

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,89 @@
 // Page should have a mapping of all the cart data and also display quantities with a way to edit the quantity
 // You can remove from cart on this page as well
 // Access to check out from here as well
-
+import useShopUser from "./context/UserContext.jsx";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  Button,
+  ButtonGroup,
+} from "@chakra-ui/react";
+import { NumericFormat, numericFormatter } from "react-number-format";
 /*
 Needs to import or receive data in relation to the cart itself and other data relevant
 to the user; cart should have two functionalities, one as a guest checkout and one as a user checkout 
 user should be authenticated for checkout process
 */
 export default function Cart() {
-  /*
+  const { isLoggedIn, cart, addToCart, removeFromCart, updateProduct } =
+    useShopUser();
+
+  // console.log(`cart cart`, cart);
+  function handleCartMap(cart) {
+    return cart.map((product) => (
+      <Card
+        key={product.id}
+        direction={{ base: "column", sm: "row" }}
+        className="cart-product"
+      >
+        <Image
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src={product.image}
+          alt={product.title}
+        />
+        <Stack className="cart-product-content">
+          <CardBody>
+            <Heading size="md">{product.title}</Heading>
+            <Text py="2">${product.price}</Text>
+          </CardBody>
+          <CardFooter>
+            <Button
+              variant="solid"
+              colorScheme="blue"
+              onClick={() => {
+                removeFromCart(product);
+              }}
+            >
+              Remove from cart
+            </Button>
+            {product.qty > 1 && (
+              <Button onClick={() => updateProduct(product)}>-</Button>
+            )}
+            <h4>{product.qty}</h4>
+            <Button onClick={() => addToCart(product)}>+</Button>
+          </CardFooter>
+        </Stack>
+        <p>Product Total: ${product.price * product.qty}</p>
+      </Card>
+    ));
+  }
+  return (
+    <div id="cart">
+      {handleCartMap(cart)}
+      <div>
+        <p>Cart Total: </p>
+        <NumericFormat
+          value={cart.reduce((acc, cv) => {
+            let productTotal = cv.price * cv.qty;
+            return acc + productTotal;
+          }, 0)}
+          prefix={"$"}
+          decimalScale={2}
+          thousandSeparator={true}
+          displayType={"text"}
+        />
+      </div>
+    </div>
+  );
+}
+/*
   
  cart.map(item => {
   <>
@@ -24,4 +99,3 @@ export default function Cart() {
  button to direct to check out 
 
   */
-}
