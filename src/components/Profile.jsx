b94a1cb42f1f0c0ca6672e14775c329c32f21a3a
fmagmych <mychaelmagnuson@gmail.com>
2023-09-13 21:36:33 -0500

    Setting the default export and sudocode for building a profile page and features, this is not required for MVP but will be built out later for the db and api functionality

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+/*
+Needs imports to be able to work: useEffect and useNavigate at minimum
+*/
+
+export default function Profile() {
+  /*
+
+  use a useEffect to be able to display the profile data for the logged in user
+  useEffect(()=>{
+    async function profileFetch() {
+      try {
+        const data = await fetchProfile(userToken);
+
+        return data;
+      } catch(err) {
+         console.error(err);
+      }
+    }
+  },[]);
+
+  return (
+    <>
+      display relevant profile data
+      edit user data (email, name, password, username)
+    </>
+  )
+  */
+}
