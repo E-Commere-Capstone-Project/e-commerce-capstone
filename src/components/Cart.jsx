fe198e261ab71360f2d70293ea1715397fc1127b
fmagmych <mychaelmagnuson@gmail.com>
2023-09-14 13:12:01 -0500

    Adding the useEffect that communicates between the cart in the context and in the local storage to make sure everything is working properly

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,7 @@
 // Page should have a mapping of all the cart data and also display quantities with a way to edit the quantity
 // You can remove from cart on this page as well
 // Access to check out from here as well
+import { useEffect } from "react";
 import useShopUser from "./context/UserContext.jsx";
 import {
   Card,
@@ -10,11 +11,9 @@ import {
   Stack,
   Heading,
   Text,
-  Divider,
   Button,
-  ButtonGroup,
 } from "@chakra-ui/react";
-import { NumericFormat, numericFormatter } from "react-number-format";
+import { NumericFormat } from "react-number-format";
 /*
 Needs to import or receive data in relation to the cart itself and other data relevant
 to the user; cart should have two functionalities, one as a guest checkout and one as a user checkout 
@@ -25,6 +24,12 @@ export default function Cart() {
     useShopUser();
 
   // console.log(`cart cart`, cart);
+
+  useEffect(() => {
+    console.log(`useEffect`, cart);
+    localStorage.setItem("localCart", JSON.stringify(cart));
+  }, [cart]);
+
   function handleCartMap(cart) {
     return cart.map((product) => (
       <Card
@@ -64,6 +69,7 @@ export default function Cart() {
       </Card>
     ));
   }
+
   return (
     <div id="cart">
       {handleCartMap(cart)}
@@ -83,19 +89,3 @@ export default function Cart() {
     </div>
   );
 }
-/*
-  
- cart.map(item => {
-  <>
-  image
-  title
-  way to change the quantity
-  a trash icon to be able to remove from cart
-  </>
- }) 
-  
- Display the total and should update in real time as items are added or removed
-
- button to direct to check out 
-
-  */
