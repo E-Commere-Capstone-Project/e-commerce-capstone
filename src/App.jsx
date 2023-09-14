2d7986f25f5a2966fb1f123763269f730c92d4df
fmagmych <mychaelmagnuson@gmail.com>
2023-09-14 11:00:15 -0500

    Making sure there is a working route for the login.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home.jsx";
 import Products from "./components/Products.jsx";
 import ViewProduct from "./components/ViewProduct.jsx";
 import Footer from "./components/Footer.jsx";
+import Login from "./components/Login.jsx";
 
 import "./App.css";
 
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:productId" element={<ViewProduct />} />
+        <Route path="/users/login" element={<Login />} />
       </Routes>
 
       <Footer />
