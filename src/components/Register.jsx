8121ea9b6517e44e00f156b3985d910ed6858c08
fmagmych <mychaelmagnuson@gmail.com>
2023-09-13 21:37:25 -0500

    Making sure the register component has a default export and sudocode on building the form and having a confirm password input

---
+++ src/components/Register.jsx
@@ -0,0 +1,29 @@
+// import useState from react;
+export default function Register() {
+  /*
+  const usernameState;
+  const passwordState;
+  const confirmPasswordState;
+
+  return (
+    form to enter username and password in order to register 
+    <form>
+    <label>
+    Username: 
+     <input value={usernameState} onChange={(e)=>setUsernameState(e.target.value)}/>
+    </label>
+    <label>
+    Password:
+    <input value={passwordState} onChange={e => setPasswordState(e.target.value)}/>
+    </label>
+    <label>
+    Confirm Password:
+    <input value={confirmPasswordState} onChange={e => setConfirmPasswordState(e.target.value)}/>
+    </label>
+    <Check if password and confirm password match before allowing the user to be able to submit the form/>
+    <Display an error message if the password and confirm password do not match/>
+    <button>Register</button>
+    </form>
+  )
+  */
+}
