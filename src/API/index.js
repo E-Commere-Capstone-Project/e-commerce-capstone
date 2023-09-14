6b2fb0ab571c0860cbdcbc1ddf006116ac37b0f4
fmagmych <mychaelmagnuson@gmail.com>
2023-09-14 09:07:37 -0500

    Adding api calls for logging in, registering a new user, deleting a user, and updating a user's data

--- src/API/index.js
+++ src/API/index.js
@@ -1,6 +1,8 @@
 // USE for writing all the api calls
 const API_URL = "https://fakestoreapi.com";
 const PRODUCTS_API_URL = `${API_URL}/products`;
+const LOGIN_API_URL = `${API_URL}/auth/login`;
+const USERS_API_URL = `${API_URL}/users`;
 
 // GET all products
 export async function fetchProducts() {
@@ -30,6 +32,112 @@ export async function fetchOneProduct(productId) {
     });
     const data = await response.json();
     return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// POST User login
+export async function fetchLogin(username, password) {
+  const validUsername = "mor_2314";
+  const validPassword = "83r5^_";
+  try {
+    const response = await fetch(LOGIN_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: validUsername,
+        password: validPassword,
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(`Logged in user`, error);
+  }
+}
+
+// POST register a new user
+export async function fetchRegister(
+  email,
+  username,
+  password,
+  firstName,
+  lastName
+) {
+  try {
+    const response = await fetch(USERS_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        username: username,
+        password: password,
+        name: {
+          firstName: firstName,
+          lastName: lastName,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log(`New registered user`, result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// DELETE user
+export async function fetchDeleteUser(userId, token) {
+  try {
+    const response = await fetch(`${USERS_API_URL}/${userId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log(`User deleted`, result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// PATCH edit a users data
+export async function fetchUpdateUser(
+  userId,
+  token,
+  email,
+  username,
+  password,
+  firstName,
+  lastName,
+  phone
+) {
+  try {
+    const response = await fetch(`${USERS_API_URL}/${userId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        email: email,
+        username: username,
+        password: password,
+        name: {
+          firstName: firstName,
+          lastName: lastName,
+        },
+        phone: phone,
+      }),
+    });
+    const result = await response.json();
+    console.log(`Updated user data`, result);
   } catch (error) {
     console.error(error);
   }
