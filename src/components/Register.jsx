eaca4f01e06ba6f78c25320f4baa4d0025e8a8cf
fmagmych <mychaelmagnuson@gmail.com>
2023-09-16 10:12:20 -0500

    Building out the framework for the register component to be able to create a new user

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,100 @@
-// import useState from react;
+import { useState } from "react";
+import { fetchRegisterUser } from "../API/index.js";
+import { Form, useNavigate } from "react-router-dom";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+// import useShopUser from "./context/UserContext.jsx";
+
 export default function Register() {
-  /*
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const isEmpty = password === "";
+
+  // const { changeIsLoggedIn } = useShopUser();
+
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const user = await fetchRegisterUser(
+      email,
+      username,
+      password,
+      firstName,
+      lastName
+    );
+    console.log(`New Registered User`, user);
+    // onSetIsLoggedIn(true);
+    setUsername("");
+    setPassword("");
+    setTimeout(() => {
+      navigate("/users/login");
+    }, 2000);
+  }
+
+  return (
+    <form method="POST" onSubmit={handleSubmit} id="register-form">
+      <FormControl isRequired>
+        <FormLabel>First Name</FormLabel>
+        <Input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Last Name</FormLabel>
+        <Input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Email Address</FormLabel>
+        <Input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel>Username</FormLabel>
+        <Input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </FormControl>
+      <FormControl isInvalid={isEmpty}>
+        <FormLabel>Password</FormLabel>
+        <Input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {!isEmpty ? (
+          ""
+        ) : (
+          <FormErrorMessage>Password is required.</FormErrorMessage>
+        )}
+      </FormControl>
+
+      <Button>Register Now</Button>
+    </form>
+  );
+}
+
+/*
   const usernameState;
   const passwordState;
   const confirmPasswordState;
@@ -26,4 +120,3 @@ export default function Register() {
     </form>
   )
   */
-}
