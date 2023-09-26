d2b2fa2242887996376486010452573081c66e66
fmagmych <mychaelmagnuson@gmail.com>
2023-09-26 13:23:34 -0500

    Adjusting current routes, adding new routes, and changing the api url now that the custom backend is deployed

--- src/API/index.js
+++ src/API/index.js
@@ -1,8 +1,9 @@
 // USE for writing all the api calls
-const API_URL = "https://fakestoreapi.com";
+const API_URL = "https://e-commerce-server-capstone.onrender.com/api";
 const PRODUCTS_API_URL = `${API_URL}/products`;
-const LOGIN_API_URL = `${API_URL}/auth/login`;
+const LOGIN_API_URL = `${API_URL}/users/login`;
 const USERS_API_URL = `${API_URL}/users`;
+const CART_API_URL = `${API_URL}/cart`;
 
 // GET all products
 export async function fetchProducts() {
@@ -37,10 +38,112 @@ export async function fetchOneProduct(productId) {
   }
 }
 
+// POST new product
+export async function fetchAddNewProduct(
+  token,
+  name,
+  description,
+  category_id,
+  price,
+  product_image,
+  quantity
+) {
+  try {
+    const response = await fetch(PRODUCTS_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name: name,
+        description: description,
+        category_id: category_id,
+        price: price,
+        product_image: product_image,
+        quantity: quantity,
+      }),
+    });
+    const result = await response.json();
+    console.log("API: ADDED NEW PRODUCT ", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// PATCH product info
+export async function fetchPatchProduct(
+  token,
+  productId,
+  name,
+  description,
+  category_id,
+  price,
+  product_image,
+  quantity
+) {
+  try {
+    const response = await fetch(`${PRODUCTS_API_URL}/${productId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name: name,
+        description: description,
+        category_id: category_id,
+        price: price,
+        product_image: product_image,
+        quantity: quantity,
+      }),
+    });
+    const result = await response.json();
+    console.log("API: UPDATED PRODUCT ", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// DELETE product
+export async function fetchDeleteProduct(token, productId) {
+  try {
+    const response = await fetch(`${PRODUCTS_API_URL}/${productId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log("API: DELETED PRODUCT ", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//GET User account
+export async function fetchAccount(token) {
+  try {
+    const response = await fetch(`${USERS_API_URL}/account`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 // POST User login
 export async function fetchLogin(username, password) {
-  const validUsername = "mor_2314";
-  const validPassword = "83r5^_";
   try {
     const response = await fetch(LOGIN_API_URL, {
       method: "POST",
@@ -48,8 +151,8 @@ export async function fetchLogin(username, password) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        username: validUsername,
-        password: validPassword,
+        username: username,
+        password: password,
       }),
     });
     const result = await response.json();
@@ -62,30 +165,28 @@ export async function fetchLogin(username, password) {
 
 // POST register a new user
 export async function fetchRegisterUser(
-  email,
   username,
   password,
   firstName,
-  lastName
+  lastName,
+  telephone
 ) {
   try {
-    const response = await fetch(USERS_API_URL, {
+    const response = await fetch(`${USERS_API_URL}/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        email: email,
         username: username,
         password: password,
-        name: {
-          firstName: firstName,
-          lastName: lastName,
-        },
+        first_name: firstName,
+        last_name: lastName,
+        telephone: telephone,
       }),
     });
     const result = await response.json();
-    console.log(`New registered user`, result);
+    console.log(`API: New registered user`, result);
     return result;
   } catch (error) {
     console.error(error);
@@ -111,36 +212,177 @@ export async function fetchDeleteUser(userId, token) {
 }
 
 // PATCH edit a users data
-export async function fetchUpdateUser(
-  userId,
-  token,
-  email,
-  username,
-  password,
-  firstName,
-  lastName,
-  phone
-) {
+// export async function fetchUpdateUser(
+//   userId,
+//   token,
+//   email,
+//   username,
+//   password,
+//   firstName,
+//   lastName,
+//   phone
+// ) {
+//   try {
+//     const response = await fetch(`${USERS_API_URL}/${userId}`, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify({
+//         email: email,
+//         username: username,
+//         password: password,
+//         name: {
+//           firstName: firstName,
+//           lastName: lastName,
+//         },
+//         phone: phone,
+//       }),
+//     });
+//     const result = await response.json();
+//     console.log(`Updated user data`, result);
+//     return result;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// GET users cart
+export async function fetchCart(token) {
   try {
-    const response = await fetch(`${USERS_API_URL}/${userId}`, {
+    const response = await fetch(CART_API_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log("API: USER CART:", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// POST add to cart
+export async function fetchAddToCart(token, productId, quantity) {
+  try {
+    const response = await fetch(CART_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        productId: productId,
+        quantity: quantity,
+      }),
+    });
+    const result = await response.json();
+    console.log("API: ADDED TO CART:", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// PATCH update an item from the cart
+export async function fetchUpdateCart(token, productId, quantity) {
+  try {
+    const response = await fetch(CART_API_URL, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({
-        email: email,
-        username: username,
-        password: password,
-        name: {
-          firstName: firstName,
-          lastName: lastName,
-        },
-        phone: phone,
+        productId: productId,
+        quantity: quantity,
       }),
     });
     const result = await response.json();
-    console.log(`Updated user data`, result);
+    console.log("API: UPDATED ITEM IN CART:", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// DELETE a single item from the cart
+export async function fetchRemoveFromCart(token, productId) {
+  try {
+    const response = await fetch(CART_API_URL, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        productId: productId,
+      }),
+    });
+    const result = await response.json();
+    console.log("API: REMOVED FROM CART: ", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// DELETE entire users cart
+export async function fetchRemoveCartAll(token) {
+  try {
+    const response = await fetch(CART_API_URL, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log("API: REMOVED ALL FROM CART: ", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// ADMIN LOGIN
+// POST
+export async function fetchAdminLogin(username, password) {
+  try {
+    const response = await fetch(`${USERS_API_URL}/admin/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    const result = await response.json();
+    console.log("API: ADMIN LOGIN ", result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// GET Admin by id
+export async function fetchAdminUser(token) {
+  try {
+    const response = await fetch(`${USERS_API_URL}/admin/account`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log("API: ADMIN USER ", result);
     return result;
   } catch (error) {
     console.error(error);
